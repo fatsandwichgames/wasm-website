847421a678adf54a0963ed0c0f5f8268eb1d0491
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2020-12-13 20:54:16 +1000

    Add terms of use

--- Sources/Games/Views/Legal/PrivacyPolicy.swift
+++ Sources/Games/Views/Legal/PrivacyPolicy.swift
@@ -23,7 +23,7 @@ struct PrivacyPolicy: View {
                     If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
                     The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at \(product) unless otherwise defined in this Privacy Policy.
                     """)
-                }
+                }.padding()
                 Group {
                     Text("Information Collection and Use").font(.title)
                     MultilineText("""
@@ -100,7 +100,7 @@ struct PrivacyPolicy: View {
                         Text("Brows applications")
                     }
                 }.padding()
-            }
+            }.padding()
         }.padding()
     }
 }
